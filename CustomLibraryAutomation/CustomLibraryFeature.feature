eda0e145fecff47263be5fd65e4fd6a6e80eb508
calvicontinousdelivery <g.baindur@prowareness.nl>
2014-08-28 11:29:12 +0530

    test

---
+++ CustomLibraryAutomation/CustomLibraryFeature.feature
@@ -0,0 +1,11 @@
+﻿Feature: CustomLibraryFeature to Test custom library
+
+@Category1
+Scenario Outline: Add two numbers
+	Given Enter <Number1> and <Number2> in first text box	
+	When Click on Add button	
+	Then Verify that result is <Result> in the result textbox
+
+Examples: 
+| Number1 | Number2 | Result |
+| 6       | 2       | 8      |
---
+++ CustomLibraryAutomation/CustomLibraryFeatureSteps.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ArtOfTest.WebAii.Core;
+using ArtOfTest.WebAii.TestTemplates;
+using TechTalk.SpecFlow;
+using Telerik.WebAii.Controls.Html;
+
+namespace CustomLibraryAutomation
+{
+    [Binding]
+    public class CustomLibraryFeatureSteps 
+    {       
+        
+
+        [Given(@"Enter (.*) and (.*) in first text box")]
+        public void GivenEnterAndInFirstTextBox(int p0, int p1)
+        {
+            //_radTextBox = 
+     
+
+        [Then(@"Verify that result is (.*) in the result textbox")]
+        public void ThenVerifyThatResultIsInTheResultTextbox(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }    
+    }
+}
